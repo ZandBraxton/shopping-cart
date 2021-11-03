18b303e14d1aa4121d452eb8d3b6ad1cb03eb4a6
Braxton Zand <Zand.Braxton@gmail.com>
2021-11-03 12:56:36 -0500

    Added icon for if cart is empty or not, also shows how many items are in cart

--- src/Components/nav.js
+++ src/Components/nav.js
@@ -5,10 +5,20 @@ import { ShoppingCart } from '@material-ui/icons'
 
 export const Navbar = ({cart, setIsVisable}) => {
    const [cartCount, setCartCount] = useState(0)
+   const [cartEmpty, setCartEmpty] = useState(true)
 
 
    useEffect(() => {
-       setCartCount(cart.length)
+    let count = 0
+    cart.map((item) => {
+        count += item.quantity
+    })
+        if (count === 0) {
+            setCartEmpty(true)
+        } else {
+            setCartEmpty(false)
+        }
+       setCartCount(count)
    }, [cart])
 
   
@@ -23,13 +33,29 @@ export const Navbar = ({cart, setIsVisable}) => {
                 <Link className="link-line" to="/shop">
                     <li>Shop</li>
                 </Link>
-                <ShoppingCart 
-                className="cart"
-                style={{fontSize: 60}}
-                onClick={() => setIsVisable(v => !v)}
-                />
-                {cartCount}
+                <div className="cart-count-container">
+                    <div className="cart-count-wrapper">
+                        <ShoppingCart 
+                            className="cart"
+                            style={{fontSize: 60}}
+                            onClick={() => setIsVisable(v => !v)}
+                        ></ShoppingCart>
+                        {cartEmpty ? '' : <span className="cart-count">{cartCount}</span>}
+                    </div>
+               
+
+                    
+        
+               
+
+                </div>
+               
+                
             </ul>
+          
+               
+         
+                
         </nav>
     )
 }
